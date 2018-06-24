8fbb75950ed174183f23aded484f67fe1768fe6d
Aditya Pudipeddi <axelonet@mac001.local>
2018-06-24 19:25:43 +0530

    Made the crossword responsive to json.php

--- json.php
+++ json.php
@@ -1,28 +1,52 @@
 <?php
-$items  = array(    
+$items  = array(
     array(
-        'item_id'   => 4,
-        'direction' => 'down',
-        'clue'      => 'Memakan begitu saja tanpa nasi',
-        'answer'    => 'gado',
-        'start_x'   => 3,
-        'start_y'   => 0
-    ),
-    array(
-        'item_id'   => 3,
+        'item_id'   => 1,
         'direction' => 'across',
-        'clue'      => 'Alat pembayaran',
-        'answer'    => 'uang',
+        'clue'      => 'PR',
+        'answer'    => 'sarthak',
         'start_x'   => 0,
-        'start_y'   => 0
+        'start_y'   => 6
     ),
     array(
         'item_id'   => 2,
         'direction' => 'down',
-        'clue'      => 'Undang-Undang Dasar',
-        'answer'    => 'uud',
-        'start_x'   => 0,
+        'clue'      => 'Events',
+        'answer'    => 'somansh',
+        'start_x'   => 4,
         'start_y'   => 0
+    ),
+    array(
+        'item_id'   => 3,
+        'direction' => 'down',
+        'clue'      => 'Working-committee',
+        'answer'    => 'prakrit',
+        'start_x'   => 6,
+        'start_y'   => 3
+    ),
+    array(
+        'item_id'   => 4,
+        'direction' => 'down',
+        'clue'      => 'Technical',
+        'answer'    => 'aditya',
+        'start_x'   => 1,
+        'start_y'   => 6
+    ),
+    array(
+        'item_id'   => 5,
+        'direction' => 'across',
+        'clue'      => 'Secretary',
+        'answer'    => 'arun',
+        'start_x'   => 1,
+        'start_y'   => 11
+    ),
+    array(
+        'item_id'   => 6,
+        'direction' => 'across',
+        'clue'      => 'Editorial',
+        'answer'    => 'roop',
+        'start_x'   => 3,
+        'start_y'   => 1
     )
 );
 echo json_encode($items);
